3a7521a49c6b196dbb8f2254e4a589e1d2c60312
JanCarlos Acevedo <jancarlos92@me.com>
2019-01-23 15:31:49 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+require 'pry'
+
+def dictionary
+ { "hello" => "hi",
+["to, two, too"] => "2", 
+["for, four"] => "4",
+'be' => "b",
+'you' => "u",
+"at" => "@" ,
+"and" => "&"}
+
+end
+
+def word_substituter(tweet)
+  new_tweet = []
+  
+  new_tweet << tweet.split(" ")
+  
+  new_tweet.map do |word| puts word == dictionary.keys
+ 
+ end.join(" ")
+ 
+ 
+ 
+ end
+end
+
